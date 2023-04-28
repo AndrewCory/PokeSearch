87a3568326f1e843c2a56c0cdfbcae46f74ee7d1
AndrewCory <92880289+AndrewCory@users.noreply.github.com>
2023-04-27 22:27:56 -0400

    Add files via upload
    latest, final edition of code

--- BTree.h
+++ BTree.h
@@ -1,11 +1,34 @@
 #include "Block.h"
 
+//B Tree Properties
+//1. Each node contains a block containing multiple keys, at most I
+	//this is achieved in Block.h which allows us to create blocks that split after holding I values
+
+//2. B Trees are N-Ary trees, each node has up to n children. They follow BST properties
+	//this is also achieved with Block.insert() which splits when more than n children are held
+
+//3. Tree building is bottom up
+	//this is achieved in Block.insert() as well as the insert pokemon are inserted into the bottom only
+
+//4. Order 'n' tree has at most n children
+	//see above for part 2
+
+//5. Leaves are the same depth
+	//achieved through the bottom up build
+
+//6. Non leaf nodes up to n-1 keys, all keys in sorted order
+	//this is achieved through the nature of the Block.h file as when inserting and splitting, once
+	//a non leaf node has n-1 keys it must split because that means it will have too many children
+
 class BTree
 {
 private:
+	//holds a root pointer and a string representing 
+	//what value is being searchef for in the tree
 	Block* root;
 	string searchType;
 
+	//changes the root of the tree
 	void appendRoot(Block* b)
 	{
 		root = b;
@@ -22,8 +45,10 @@ public:
 	//inserts a pokemon P into the tree, sorting and splitting are done for you
 	void insert(Pokemon* p)
 	{
+		//updates the index of the pokemon using the searchType field
 		p->updateSearchedVal(searchType);
 
+		//if no blocks yet, insert one
 		if (root == nullptr)
 		{
 			Block* pushBlock;
@@ -34,8 +59,10 @@ public:
 		}
 		Block* b = nullptr;
 
+		//if there is a block, start insertion at the root
 		b = root->insert(p, false);
 
+		//if a value was returned from insert, it was a value we have to delete as a split occured
 		if (b != nullptr)
 		{
 			if (root->getParent() != nullptr)
--- Pokemon.h
+++ Pokemon.h
@@ -3,23 +3,71 @@
 #include <vector>
 using namespace std;
 
+//a pokemon object represents a singular unique pokemon to be inserted into either a heap or a tree
 struct Pokemon {
 private:
-    int hp;
+    //pokemon of the same name will have the same pokedex number, so this value is not unique
     int pokedexNum;
+
+    //the name and type of pokemon are stored as string values
     string name;
     string type1;
     string type2;
+
+    //the following integers represent the stats of a particular pokemon
+    //two pokemon of the same name will still have randomly generated stats
+    int hp;
     int att;
     int def;
     int spcAtt;
     int spcDef;
     int speed;
 
+    //searchedVal is used as an index when inserting into or searching within a tree or heap
+    //it represents that pokemons index
+    //for instance if you are sorting pokemon by health then searchedVal is equal to pokemon health
     int searchedVal;
 
-    int total = hp + att + def + spcAtt + spcDef + speed;
 public:
+
+    //getters for each field
+    int getSearchedValue()
+    {
+        return searchedVal;
+    }
+    int getPokedexNum() {
+        return pokedexNum;
+    }
+    int getAtt() {
+        return att;
+    }
+    int getDef() {
+        return def;
+    }
+    int getSpcAtt() {
+        return spcAtt;
+    }
+    int getSpcDef() {
+        return spcDef;
+    }
+    int getSpeed() {
+        return speed;
+    }
+    int getHealth() {
+        return hp;
+    }
+    string getName() {
+        return name;
+    }
+    string getType1() {
+        return type1;
+    }
+    string getType2() {
+        return type2;
+    }
+
+    //similar to a getter, this method essentially takes in a string input in order to 
+    //decide what kind of value searchedVal holds
     void updateSearchedVal(string type)
     {
         if (type == "health")
@@ -38,98 +86,36 @@ public:
             searchedVal = speed;
     }
 
-    int getSearchedValue()
-    {
-        return searchedVal;
-    }
-
-    int getPokedexNum() {
-        return pokedexNum;
-    }
-
-    int getAtt() {
-        return att;
-    }
-
-    int getDef() {
-        return def;
-    }
-
-    int getSpcAtt() {
-        return spcAtt;
-    }
-
-    int getSpcDef() {
-        return spcDef;
-    }
-
-    int getSpeed() {
-        return speed;
-    }
-
-    int getHealth() {
-        return hp;
-    }
-
-    int getTotal() {
-        return total;
-    }
-
-    string getName() {
-        return name;
-    }
-
-    string getType1() {
-        return type1;
-    }
-
-    string getType2() {
-        return type2;
-    }
-
+    //setters for each field
     void setPokedexNum(int in) {
         pokedexNum = in;
-        
-    }
 
+    }
     void setAtt(int in) {
         att = in;
     }
-
     void setDef(int in) {
         def = in;
     }
-
     void setSpcAtt(int in) {
         spcAtt = in;
     }
-
     void setSpcDef(int in) {
         spcDef = in;
     }
-
     void setSpeed(int in) {
         speed = in;
     }
-
     void setHealth(int in) {
         hp = in;
     }
-
-    void setTotal(int in) {
-        total = in;
-    }
-
     void setName(string in) {
         name = in;
     }
-
     void setType1(string in) {
         type1 = in;
     }
-
     void setType2(string in) {
         type2 = in;
     }
 };
-
